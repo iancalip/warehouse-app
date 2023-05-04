ff9f1573deef9afa6a694706d6915ed56a0d7be5
iancalip <ianllima@live.com>
2023-05-04 00:10:48 -0300

    order model

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+    before_action :set_order, only: [:show]
+    before_action :authenticate_user!
+
+    def new
+        @order = Order.new
+        @warehouses = Warehouse.all
+        @suppliers = Supplier.all
+    end
+
+    def create
+        @order = Order.new(order_params)
+        @order.user = current_user
+        if @order.save
+            redirect_to @order, notice: 'Pedido registrado com sucesso.'
+        else
+            flash.now[:notice] = 'Pedido não registrado, preencha todos os campos.'
+            render :new
+        end
+    end
+
+    def show
+    end
+
+
+
+    private
+
+    def set_order
+        @order = Order.find(params[:id])
+    end
+
+    def order_params
+        params.require(:order).permit(:warehouse_id, :supplier_id, :estimated_delivery_date)
+    end
+end
---
+++ spec/system/order/user_register_order_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um pedido'do
+
+    it 'e deve estar autenticado' do
+        #Arrange
+        #Act
+        visit root_path
+        click_on 'Registrar Pedido'
+
+        #Assert
+        expect(current_path).to eq new_user_session_path
+    end
+
+    it 'com sucesso' do
+        #Arrange
+        user = User.create!(name: 'Ian', email: 'ian@email.com', password: 'password')
+        Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', state: 'RJ', area: 60_000,
+                        address: 'Avenida do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
+        warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', state: 'SP', area: 100_000,
+                                    address: 'Avenida aeroporto, 1000', cep: '15000-000',
+                                    description: 'Galpão para cargas internacionais')
+        Supplier.create!(corporate_name: 'Samsung Electronics Inc', brand_name: 'Samsung', registration_number: '1234567891111',
+                                    city: 'São Paulo', state: 'SP', address: 'Endereço', email: 'contato@samsung.com.br', telephone: '1188888-8888')
+        supplier = Supplier.create!(corporate_name: 'LG Electronics Inc', brand_name: 'LG', registration_number: '1234567890000',
+                                    city: 'Rio de Janeiro', state: 'RJ', address: 'Endereço', email: 'contato@lg.com.br', telephone: '1199999-9999')
+        #Act
+        visit root_path
+        login_as(user)
+        click_on 'Registrar Pedido'
+        select warehouse.name, from: 'Galpão Destino'
+        select supplier.corporate_name, from: 'Fornecedor'
+        fill_in 'Data Prevista de Entrega', with: '20/12/2023'
+        click_on 'Gravar'
+
+        #Assert
+        expect(page).to have_content 'Pedido registrado com sucesso'
+        expect(page).to have_content 'Galpão Destino: Aeroporto SP'
+        expect(page).to have_content 'Fornecedor: LG Electronics Inc'
+        expect(page).to have_content 'Data Prevista de Entrega: 20/12/2023'
+        expect(page).to have_content 'Usuário responsável: Ian | ian@email.com'
+        expect(page).not_to have_content 'Galpão Maceio'
+        expect(page).not_to have_content 'Samsung Electronics Inc'
+    end
+end
